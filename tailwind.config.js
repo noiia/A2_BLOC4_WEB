0a87f45109426ac19dfdeff0cb1ce9f6ec867cef
Noia <49865944+noiia@users.noreply.github.com>
2024-02-07 09:16:24 +0100

    création accueil

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*/.{php,js}"],
+  theme: {
+    fontFamily: {
+      display: ['Inter', 'system-ui', 'sans-serif'],
+      body: ['Inter', 'system-ui', 'sans-serif'],
+    },
+    colors: {
+      green: {
+        header: '#94C676',
+        true_btn: '#CAEDA7',
+        button: '#39A325',
+        filter: '#66B95A',
+        text_bg: '#83AF68',
+      },
+      secondary: {
+        white: '#F4F2EE',
+        grey : '#B9B9B9',
+        filter_grey: '#CBCBCB',
+        beige: '#EDE5A6',
+        black: "#000000",
+      },
+    },
+  },
+  plugins: [],
+}
+
