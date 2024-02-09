d30a636fb935ec69e65390f3a4fdfd6bfa16f554
Noia <49865944+noiia@users.noreply.github.com>
2024-02-09 08:52:18 +0100

    graphical update for runway containers

--- Accueil/Accueil.php
+++ Accueil/Accueil.php
@@ -5,33 +5,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <link href="./output.css" rel="stylesheet">
+    <link href="output.css" rel="stylesheet">
     <title>Inter'net - Accueil</title>
 </head>
 
 <body>
     <div class="runway-container">
 
-        <a href="www.google.com">
+        <button class="container" id="container">
             <span>
-                <div class="container-intern">
+                <div class="container-intern" id="container-intern">
                     <div class="container-div-para">
                         <h1 class="container-h1">Commercial H/F - Bac+2</h1>
                         <p class="container-paragraph">Orange - Reims</p>
                         <p class="container-paragraph">Début = 31/01/2024</p>
                         <p class="container-paragraph">3 semaines</p>
-                        <span class="container-skills">
+                        <div class="container-skills">
                             <ul>
                                 <li>Js</li>
                                 <li>Js</li>
                                 <li>Js</li>
                             </ul>
-                        </span>
+                        </div>
                     </div>
                 </div>
             </span>
-        </a>
-
+        </button>
+        <button class="container">
+            <span>
+                <div class="container-intern" id="container-intern">
+                    <div class="container-div-para">
+                        <h1 class="container-h1">Commercial H/F - Bac+2</h1>
+                        <p class="container-paragraph">Orange - Reims</p>
+                        <p class="container-paragraph">Début = 31/01/2024</p>
+                        <p class="container-paragraph">3 semaines</p>
+                        <div class="container-skills">
+                            <ul>
+                                <li>Js</li>
+                                <li>Js</li>
+                                <li>Js</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </span>
+        </button>
     </div>
 </body>
 
