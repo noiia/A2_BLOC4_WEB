d5e1a589e2aedaafcb3b37ad16bf57a4371871bf
Noia <49865944+noiia@users.noreply.github.com>
2024-02-09 14:48:38 +0100

    first validated version of runway bubbles style

---
+++ Welcome/Welcome.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link href="output.css" rel="stylesheet">
+    <title>Inter'net - Accueil</title>
+</head>
+
+<body>
+    <div class="runway-container">
+
+        <button class="container" id="container">
+            <span>
+                <div class="container-intern" id="container-intern">
+                    <div class="container-div-para">
+                        <h1 class="container-h1">Commercial H/F - Bac+2</h1>
+                        <p class="container-paragraph">Orange - Reims</p>
+                        <p class="container-paragraph">Début = 31/01/2024</p>
+                        <p class="container-paragraph">3 semaines</p>
+                        <div class="container-skills">
+                            <ul>
+                                <li>Js</li>
+                                <li>Js</li>
+                                <li>Js</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </span>
+        </button>
+        <button class="container" id="container">
+            <span>
+                <div class="container-intern" id="container-intern">
+                    <div class="container-div-para">
+                        <h1 class="container-h1">Commercial H/F - Bac+2</h1>
+                        <p class="container-paragraph">Orange - Reims</p>
+                        <p class="container-paragraph">Début = 31/01/2024</p>
+                        <p class="container-paragraph">3 semaines</p>
+                        <div class="container-skills">
+                            <ul>
+                                <li>Js</li>
+                                <li>Js</li>
+                                <li>Js</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </span>
+        </button>
+        <button class="container" id="container">
+            <span>
+                <div class="container-intern" id="container-intern">
+                    <div class="container-div-para">
+                        <h1 class="container-h1">Commercial H/F - Bac+2</h1>
+                        <p class="container-paragraph">Orange - Reims</p>
+                        <p class="container-paragraph">Début = 31/01/2024</p>
+                        <p class="container-paragraph">3 semaines</p>
+                        <div class="container-skills">
+                            <ul>
+                                <li>Js</li>
+                                <li>Js</li>
+                                <li>Js</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </span>
+        </button>
+    </div>
+</body>
+
+<script>
+    var container = document.getElementById("container");
+    container.addEventListener('click', function () {
+        container.classList.add("clicked");
+        console.log("clicked");
+    });
+</script>
+<link href="http://e0.extreme-dm.com/s9.g?login=daoudi&jv=n&j=y&srw=1920&srb=24&l=https%3A//www.adaoudi.com/">
+
+</html>
