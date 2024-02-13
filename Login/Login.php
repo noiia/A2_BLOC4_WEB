f278c750b55940cd0e5a84440ab53dfc8cb4eebf
Noia <49865944+noiia@users.noreply.github.com>
2024-02-13 09:58:23 +0100

    save2

--- Login/Login.php
+++ Login/Login.php
@@ -4,16 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="output.css">
     <title>Login</title>
 </head>
 
 <body>
-    <div>
-        <h1>Nom d'utilisateur</h1>
-        <input type="text" class="username" id="username">
-        <h1>Mot de passe</h1>
-        <input type="text" class="password" id="password">
-        <input type="button" value="Connexion">
+    <div class="container-login">
+        <img src="../../Assets/Icones/logo_seul_blanc.png" class="logo" alt="logo">
+        <h1 class="title-username">Nom d'utilisateur</h1>
+        <input class="tb-login" type="text" class="username" id="username">
+        <h1 class="title-password">Mot de passe</h1>
+        <input class="tb-password" type="text" class="password" id="password">
+        <input class="connexion-btn" type="button" value="Connexion">
     </div>
 
 </body>
