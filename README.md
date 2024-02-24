6f6f0057f22061b530921193a47b059ee02d30ec
stephanCESI <stephan.laumel@viacesi.fr>
2024-02-24 19:25:03 +0100

    responsive internship details

---
+++ README.md
@@ -0,0 +1,29 @@
+# WEB Project
+
+## Introduction
+
+## Responsive
+
+### Welcome
+
+For the welcome we added 4 breakpoints due to the different elements of the page. During this explanation we will begin by describing the full-size page (as 16/9 ratio 1080\*1920 res) and we will finish with the lowest (as cellphone, above 500 pixels of width).
+
+The different break points are the following one :
+
+- sm : 500px
+- md : 768px
+- ml : 850px
+- lg : 1024px
+- xl : 1280px
+- 2xl : 1536px
+
+The full page is designed in 3 parts :
+
+- the filters
+- the runway
+- the detailled container
+
+When you reduce the page to 2xl size, the filter desappear of the left and goes to the top of the runway the page is divided as <insert ratio>.
+Next, when you reduce the size to xl size, the detailled container desappear and the runway goes in the middle of the screen. When you click on an element of the runway, it opens a page in full screen size with the details of the intern.
+Above ml size, the menu disappear to become a burger menu.
+Finally, above sm size, all the element reduce their size to be as compatible as much with cellphone screens.
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,14 @@ module.exports = {
       },
       green: {
         header: '#94C676',
+        linear: '#104E07',
+        linear2: '#22C55E',
         true_btn: '#CAEDA7',
         button: '#39A325',
         filter: '#66B95A',
         text_bg: '#83AF68',
         shutter: '#66B95A',
-        1 : '#15751E',
-        2 : '#39A325',
-        3 : '#69B254',
-        shutter: '#66B95A',
+        volet: '#68BC5C',
         1 : '#15751E',
         2 : '#39A325',
         3 : '#69B254',
@@ -28,20 +27,10 @@ module.exports = {
         white: '#F4F2EE',
         true_white: '#FFFFFF',
         login_bg: '#F4F2EE',
-        true_white: '#FFFFFF',
-        login_bg: '#F4F2EE',
         grey : '#B9B9B9',
         filter_grey: '#CBCBCB',
         footer_grey: '#A4A4A4', 
-        footer_grey: '#A4A4A4', 
         beige: '#EDE5A6',
-        arrow_header: '#C9C9C9',
-        pp_grey: '#929292',
-        scroll_bar_grey: '#7B7B7B',
-        left_bg_grey: '#D9D9D9',
-        footer_black: '#252525', 
-        true_black: "#000000",
-        transparent: 'transparent',
         arrow_header: '#C9C9C9',
         pp_grey: '#929292',
         scroll_bar_grey: '#7B7B7B',
@@ -59,6 +48,11 @@ module.exports = {
       'xl' : '1280px',
       '2xl' : '1536px',
     },
+    extend:{
+      backgroundImage:{
+        'login': "url('../Assets/Background/login.gif')"
+      }
+    }
   },
   plugins: [
     require('tailwindcss'),
