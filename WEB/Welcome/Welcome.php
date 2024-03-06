649520434001e865b31f060d4fe6d45bad719095
Mathice <mathice.leonie91@gmail.com>
2024-03-06 15:47:23 +0100

    gestion des etudiants (sans responsive)

--- WEB/Welcome/Welcome.php
+++ WEB/Welcome/Welcome.php
@@ -2,16 +2,16 @@
 function send_mail(){
     $to = "mathice.leonie@viacesi.fr";
     $to_student = "mathice.leonie91@gmail.com";
-    $subject = "Postulation à l'offre de stage ...";
+    $subject = "Postulation à l'offre de stage *Commercial H/F* de **Orange**";
     if ($_POST["motivation_letter"]!="") {$message = $_POST["motivation_letter"];}
     else {$message = "Un étudiant a postulé à votre offre de stage!!!";}
     $message = wordwrap($message,70,"\n");
-    $headers = "FROM: "+$to_student."\r\n".'X-Mailer: PHP/' . phpversion();
+    $headers = "FROM: ".$to_student."\r\n".'X-Mailer: PHP/' . phpversion();
     
     $send = mail($to, $subject, $message, $headers);
 
     if ($send==true) {$message_student = "Votre candidature a bien été envoyé";}
-    else {$message_student = "Votre candidature n'a pas été envoyé voici son mail: "+$to;}
+    else {$message_student = "Votre candidature n'a pas été envoyé voici son mail: ".$to;}
 
     mail($to_student, $subject, $message_student);
 }
