77d4c6806ced8f8f02e45f23425deee17245fc55
Mathice <mathice.leonie91@gmail.com>
2024-03-07 11:58:30 +0100

    Gestion des entreprises (manque lieu de l'entreprise)

---
+++ WEB/Company/Company.php
@@ -0,0 +1,36 @@
+<?php
+function check_role(){
+    $role =3;
+    switch ($role) {
+        case 2 :
+            echo ' <div class="left_container" id="left_array">
+            <p>Pilote</p>
+            <ul>
+                <li><a href="../Profile/Profile.html">Mon profil</a></li>
+                <li><a href="../Students/Students.html">Mes étudiants</a></li>
+                <li><a id="actual_page">Mes entreprises</a></li>
+                <li><a href="../Internship/Internship.html">Mes stages</a></li>
+            </ul>
+        </div>';
+        break;
+        case 3 : 
+            echo '<div class="left_container" id="left_array">
+            <p>Admin</p>
+            <ul>
+                <li><a href="../Profile/Profile.html">Mon profil</a></li>
+                <li><a href="../Students/Students.html">Etudiants</a></li>
+                <li><a href="../Pilote/Pilote.html">Pilotes</a></li>
+                <li><a id="actual_page">Entreprises</a></li>
+                <li><a href="../WishList/WishList.html">WishList</a></li>
+            </ul>
+        </div>';
+        break;
+        default :
+        echo '';
+    }
+}
+
+if ($_POST['action'] == 'role')
+{
+    check_role();
+}
