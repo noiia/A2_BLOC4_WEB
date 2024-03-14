c299014e5dc355931847a3885b3a374b5e1e3cd1
Mathice <mathice.leonie91@gmail.com>
2024-03-14 10:33:47 +0100

    fin Internship

--- WEB/Internship/Internship.js
+++ WEB/Internship/Internship.js
@@ -2,3 +2,36 @@ function toggle_search(){
     document.querySelector(".search_list-companies").classList.toggle('mobile-search_menu-menu');
     document.querySelector(".profile_company").classList.toggle('mobile-search_menu-profile');
 }
+
+var current_student = null;
+function toggle_valid(id){
+    current_student = id;
+    document.querySelector(".valid_bg").classList.toggle('active-del_block');
+}
+function valid_current_student(){
+    document.getElementById(current_student).classList.remove('black_check');
+    console.log(current_student+" à été recruté");
+    //ajouter dans la bdd
+    //envoie du mail
+    toggle_valid(current_student);
+    document.getElementById(current_student).onclick = function(){toggle_end(current_student)};
+}
+
+function toggle_end(id){
+    current_student = id;
+    document.querySelector(".end_bg").classList.toggle('active-del_block');
+}
+function end_current_student(){
+    var radio = document.querySelector('input[name=end_radio]:checked');
+    if (radio !== null){
+        document.getElementById(current_student).classList.add('black_check');
+        console.log(current_student+" à été enlevé d'un stage pour raison: "+radio.value);
+        //ajouter dans la bdd
+        //envoie du mail
+        toggle_end(current_student);
+        document.getElementById(current_student).onclick = function(){toggle_valid(current_student)};
+    } else {
+        alert("Veuillez sélectionné une valeur avant de valider");
+    }
+}
+
