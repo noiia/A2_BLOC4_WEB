fdf76d5c39bbfc9d5bd17328615ec64bfb0950b7
Mathice <mathice.leonie91@gmail.com>
2024-03-14 11:56:06 +0100

    merge avec popup (boutons: ajouter=editer / annuler=valider)

--- WEB/main/main.js
+++ WEB/main/main.js
@@ -81,6 +81,10 @@ function del_current_container(){
     container_focus = null;
 }
 
+function toggle_hide_popup(){
+    document.querySelector('.container-add-student').classList.toggle('hide_container');
+}
+
 // -------------------- fin parti gestion -------------------
 
 
