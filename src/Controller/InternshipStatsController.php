6b220b549588a74a77cd0c2fc0fe83e63f9655e7
Mathice <mathice.leonie91@gmail.com>
2024-03-31 22:26:42 +0200

    svg statsStage

--- src/Controller/InternshipStatsController.php
+++ src/Controller/InternshipStatsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Internship;
 use Doctrine\ORM\EntityManager;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -20,6 +21,23 @@ class InternshipStatsController
 
     public function InternshipStats(Request $request, Response $response): Response
     {
+        // debut svg
+        $depToNbInternship = array_count_values(array_map(
+            function ($element) {
+                return $element->locations->getDepartement();
+            },
+            $this->entityManager->getRepository(Internship::class)->findAll()
+        ));
+        $total = array_sum($depToNbInternship);
+        $svg = simplexml_load_file('../public/images/svg/Carte_vierge_départements_français.svg');
+        foreach ($depToNbInternship as $dep => $nb) {
+            $mapDep = $svg->xpath('//*[@id="dep' . $dep . '"]')[0];
+            $mapDep['fill'] = "#ffff00";
+            $mapDep['fill-opacity'] = ($nb / $total) * 5;
+        }
+        $svg->asXML('../public/images/svg/Carte_remplie_départements_français.svg');
+
+        // fin svg
         return $this->twig->render($response, 'InternshipStats/InternshipStats.html.twig');
     }
 }
