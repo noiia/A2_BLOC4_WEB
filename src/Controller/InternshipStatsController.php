51944e6fa8c239f3411a8bdfbd32deee1cd090ad
Mathice <mathice.leonie91@gmail.com>
2024-04-01 15:58:06 +0200

    apres reset

--- src/Controller/InternshipStatsController.php
+++ src/Controller/InternshipStatsController.php
@@ -20,6 +20,24 @@ class InternshipStatsController
 
     public function InternshipStats(Request $request, Response $response): Response
     {
+        // debut svg
+        $depToNbInternship = array_count_values(array_map(
+            function ($element) {
+                return $element->locations->getDepartement();
+            },
+            $this->entityManager->getRepository(Internship::class)->findAll()
+        ));
+        $total = array_sum($depToNbInternship);
+        $svg = simplexml_load_file('../public/images/svg/Carte_vierge_départements_français.svg');
+        foreach ($depToNbInternship as $dep => $nb) {
+            $mapDep = $svg->xpath('//*[@id="dep' . $dep . '"]')[0];
+            $mapDep['fill'] = "#104E07";
+            $mapDep['fill-opacity'] = ($nb / $total) * 5; //2% de stage = 100% opaque
+        }
+        $svg->asXML('../public/images/svg/Carte_remplie_départements_français.svg');
+
+        // fin svg
+
         return $this->twig->render($response, 'InternshipStats/InternshipStats.html.twig');
     }
 }
